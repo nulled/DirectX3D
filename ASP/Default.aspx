8a76c3fea6697e192d6aeebd905e07dbdd953e05
nulled <elitescripts2000@yahoo.com>
2019-04-29 15:40:21 -0700

    Added SQL connection

--- ASP/Default.aspx
+++ ASP/Default.aspx
@@ -4,13 +4,22 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>SQL Login ASP Style</title>
 </head>
 <body>
     <form id="form1" runat="server">
         <div>
-            Hello World!
+            FistName: <asp:TextBox ID="txtFirstname" runat="server" Text='<%# Eval("txtFirstname") %>'></asp:TextBox>
+            <br />
+            LastName: <asp:TextBox ID="txtLastname" runat="server"></asp:TextBox>
+            <br />
+            Phone: <asp:TextBox ID="txtPhone" runat="server"></asp:TextBox>
+            <br />
+            <br /><br />
+            <asp:Button ID="btnSubmit" runat="server" Text="Button" OnClick="btnSubmit_Click" />
         </div>
+        <asp:GridView ID="gridView" runat="server">
+        </asp:GridView>
     </form>
 </body>
 </html>
--- ASP/Default.aspx.cs
+++ ASP/Default.aspx.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
 
 public partial class _Default : System.Web.UI.Page
 {
+    SqlConnection con = new SqlConnection(@"Data Source=MATT-LAPTOP;Initial Catalog=test;User ID=sa;Password=ntws4074");
+    SqlCommand cmd;
+    SqlDataAdapter adpt;
+    DataTable dt;
+
     protected void Page_Load(object sender, EventArgs e)
     {
+        con.Open();
+        adpt = new SqlDataAdapter("SELECT * FROM users", con);
+        dt = new DataTable();
+        adpt.Fill(dt);
+        gridView.DataSource = dt;
+        gridView.DataBind();
+        con.Close();
+        txtFirstname.Text = "WOW";
+    }
 
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        con.Open();
+        cmd = new SqlCommand("INSERT INTO users VALUES('" + txtFirstname.Text + "','" + txtLastname.Text + "','" + txtPhone.Text + "')", con);
+        cmd.ExecuteNonQuery();
+        con.Close();
     }
 }
